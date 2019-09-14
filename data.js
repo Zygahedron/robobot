51ec4d80cf635c441dc57567f1a08813c6ee1352
Notnat Tlikehis <notnat.tlikehis@gmail.com>
2019-09-13 19:11:00 -0700

    oh

--- data.js
+++ data.js
@@ -9,9 +9,9 @@ exec("lua robobot/data.lua", {cwd: ".."}, (e, stdout, stderr) => {
         console.error(stderr);
     } else {
         Object.assign(data, JSON.parse(stdout))
-        data.tiles_list[data.tiles_by_name["text_gay"]].sprite = "text_gay-colored";
-        data.tiles_list[data.tiles_by_name["text_tranz"]].sprite = "text_tranz-colored";
-        data.tiles_list[data.tiles_by_name["text_enby"]].sprite = "text_enby-colored";
+        data.tiles_list[data.tiles_by_name["text_gay"]-1].sprite = "text_gay-colored";
+        data.tiles_list[data.tiles_by_name["text_tranz"]-1].sprite = "text_tranz-colored";
+        data.tiles_list[data.tiles_by_name["text_enby"]-1].sprite = "text_enby-colored";
     }
 });
 
